d6b52c6da7cddbff21c7a2dcd05f50401ba774be
josiel <moraisjosiel1@gmail.com>
2023-03-07 21:58:12 -0300

    Delete category

--- backend/src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dvsuperior.dscatalog.dto.CategoryDTO;
 import com.dvsuperior.dscatalog.entities.Category;
 import com.dvsuperior.dscatalog.repositories.CategoryRepository;
+import com.dvsuperior.dscatalog.services.exceptions.DataBaseException;
 import com.dvsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -53,6 +56,19 @@ public class CategoryService {
 		}catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found"+ id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+			
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found"+ id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation"); 
+		}		
 	}
 
 
