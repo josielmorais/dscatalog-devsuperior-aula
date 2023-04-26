b014fd2c4423fcc26cd198f9f475419c20f515e7
josiel <moraisjosiel1@gmail.com>
2023-04-26 19:11:19 -0300

    Refactor pageable

--- backend/src/main/java/com/dvsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/resources/CategoryResource.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dvsuperior.dscatalog.dto.CategoryDTO;
-import com.dvsuperior.dscatalog.entities.Category;
 import com.dvsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -37,16 +34,8 @@ public class CategoryResource {
 	}
 	
 	@GetMapping(value = "/page")
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPagepage", defaultValue = "12") Integer linePerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			) {
-		
-		PageRequest pageRequest = PageRequest.of(page,linePerPage, Direction.valueOf(direction),orderBy);
-		 		
-		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 	
--- backend/src/main/java/com/dvsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/resources/ProductResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -36,16 +36,8 @@ public class ProductResource {
 	}
 	
 	@GetMapping(value = "/page")
-	public ResponseEntity<Page<ProductDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPagepage", defaultValue = "12") Integer linePerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			) {
-		
-		PageRequest pageRequest = PageRequest.of(page,linePerPage, Direction.valueOf(direction),orderBy);
-		 		
-		Page<ProductDTO> list = service.findAllPaged(pageRequest);
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+		Page<ProductDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 	
